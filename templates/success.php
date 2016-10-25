74b0030de3bc553101d229346c797cc74dbf0c6e
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-10-25 13:34:41 +0530

    Update success.php

--- templates/success.php
+++ templates/success.php
@@ -14,29 +14,47 @@ if ( ! defined( 'ABSPATH' ) ) {
 	</head>
 	<body>
 		<?php
-		if ( ! isset( $_POST ) ) {
-			// @TODO: redirect back to checkout page.
+		if ( isset( $_REQUEST['txnid'] ) && isset( $_REQUEST['mihpayid'] ) ) {
+			$donation_id = $_REQUEST['udf1'];
+
+			if ( ! empty( $donation_id ) ) {
+				try {
+					$donation        = new Give_Payment( $donation_id );
+					$hash            = $_REQUEST['hash'];
+					$status          = $_REQUEST['status'];
+					$checkhash       = Give_Payumoney_API::get_hash( $_REQUEST );
+					$transauthorised = false;
+
+					if ( $donation->status !== 'completed' ) {
+						// Process each payment status.
+						switch ( esc_attr( $_POST['status'] ) ) {
+							case 'success':
+								$donation = new Give_Payment( absint( $_POST['udf1'] ) );
+								$donation->update_status( 'completed' );
+								give_send_to_success_page();
+								break;
+
+							case 'failure': ?>
+								<form action="<?php echo '?form-id=' . absint( $_POST['udf1'] ) . '&payment_mode = payumoney'; ?>" name="payuFailure" method="post">
+									<input type="hidden" name="payu-error-message" value="<?php echo $_POST['error_Message']; ?>">
+								</form>
+								<script>document.payuFailure.submit();</script>
+								<?php
+								break;
+
+							case 'pending':
+								break;
+						}
+					}
+				} catch ( Exception $e ) {
+					error_log( print_r( $e->getMessage(), true ) . "\n", 3, WP_CONTENT_DIR . '/debug.log' );
+				}
+			}
 		}
 
-		// Process each payment status.
-		switch ( esc_attr( $_POST['status'] ) ) {
-			case 'success':
-				$donation = new Give_Payment( absint( $_POST['udf1'] ) );
-				$donation->update_status( 'completed' );
-				give_send_to_success_page();
-				break;
-
-			case 'failure': ?>
-				<form action="<?php echo '?form-id=' . absint( $_POST['udf1'] ) . '&payment_mode = payumoney'; ?>" name="payuFailure" method="post">
-					<input type="hidden" name="payu-error-message" value="<?php echo $_POST['error_Message'];  ?>">
-				</form>
-				<script>//document.payuFailure.submit();</script>
-				<?php
-				break;
-
-			case 'pending':
-				break;
-		}
+		// Default redirect to home page.
+		wp_redirect( home_url( '/' ) );
+		exit();
 		?>
 	</body>
 </html>
