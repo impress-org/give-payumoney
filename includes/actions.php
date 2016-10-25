b79236258b079c6cbe0091abdb2c8f2fed6e1bb8
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-10-25 13:20:23 +0530

    Update give_payu_show_frontend_notices function

--- includes/actions.php
+++ includes/actions.php
@@ -23,19 +23,21 @@ add_action( 'give_payumoney_set_donation_abandoned', 'give_payumoney_set_donatio
  * @param $form_id
  */
 function give_payu_show_frontend_notices( $form_id ){
-	if ( isset( $_GET['payu-error-message'] ) && ( $form_id === $_GET['form-id'] ) ) {
+	if( $form_id !== absint( $_GET['form-id'] ) ) {
+		return;
+	}
+	
+	if ( isset( $_GET['payu-error-message'] ) ) {
 		if ( $error_message = sanitize_text_field( $_GET['payu-error-message'] )  ) {
 
 			// Show error.
 			give_output_error( $error_message , true, 'error' );
-			return;
 		}
 	}
 
 	if( isset( $_GET['process_payu_payment'] ) && 'failed' === $_GET['process_payu_payment'] ) {
 		// Show error.
-		give_output_error( __( 'Transaction failed.', 'give-payumoney' ) , true, 'error' );
-		return;
+		give_output_error( __( 'Transaction failed. Please try after some time.', 'give-payumoney' ) , true, 'error' );
 	}
 }
 add_action( 'give_pre_form_output', 'give_payu_show_frontend_notices' );
