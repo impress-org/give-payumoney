773f12aa05ff378bf67eed2383d9068d54c41374
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-02-16 14:11:23 +0530

    Unset payumoney if INR currency is not set

--- includes/actions.php
+++ includes/actions.php
@@ -42,3 +42,28 @@ function give_payu_show_frontend_notices( $form_id ) {
 }
 add_action( 'give_pre_form_output', 'give_payu_show_frontend_notices' );
 
+
+/**
+ * Validate payumoney settings.
+ * 
+ * @since 1.0
+ * @param $options
+ */
+function give_payu_validate_settings( $options ) {
+	if ( isset( $options['gateways']['payumoney'] ) && ( 'INR' !== $options['currency'] ) ) {
+		// Unset payumoney.
+		unset( $options['gateways']['payumoney'] );
+
+		// Show payment gateway disable notice to admin.
+		Give_Admin_Settings::add_error(
+			'give_payu_no_inr_currency',
+			esc_html__( 'PayUmoney payment gateway disabled because INR is not set as donation currency.', 'give-payumoney' )
+		);
+
+		// Update options.
+		update_option( 'give_settings', $options );
+	}
+}
+
+add_action( 'give_save_settings_give_settings', 'give_payu_validate_settings' );
+
