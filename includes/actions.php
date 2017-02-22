4e475fc17609073ffb7e8b2ba97a0e0459f36b4e
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-02-22 15:50:03 +0530

    Remove unused filter

--- includes/actions.php
+++ includes/actions.php
@@ -19,21 +19,6 @@ function give_payumoney_set_donation_abandoned_callback( $payment_id ) {
 
 add_action( 'give_payumoney_set_donation_abandoned', 'give_payumoney_set_donation_abandoned_callback' );
 
-/**
- * @param $form_id
- */
-function give_payu_show_frontend_notices( $form_id ) {
-	if ( ! isset( $_GET['form-id'] ) || $form_id !== absint( $_GET['form-id'] ) ) {
-		return;
-	}
-
-	if ( isset( $_GET['payu-payment-status'] ) && 'failure' === $_GET['payu-payment-status'] ) {
-		// Show error.
-		give_output_error( __( 'PayUmoney payment failed.', 'give-payumoney' ), true, 'error' );
-	}
-}
-add_action( 'give_pre_form_output', 'give_payu_show_frontend_notices' );
-
 
 /**
  * Validate payumoney settings.
