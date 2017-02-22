d44240cb853fc7c1ca239d59921e271e2a6a7e6d
Devin Walker <devin@wordimpress.com>
2017-02-22 15:59:53 -0800

    1. New plugin meta row links 2. Using doc shortlinks 3. No use for is_admin when hooking admin_init

--- includes/admin/plugin-activation.php
+++ includes/admin/plugin-activation.php
@@ -11,15 +11,13 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @since 1.0
  */
-add_action( 'admin_init', 'give_payu_activation_banner' );
-
 function give_payu_activation_banner() {
 
 	// Check for if give plugin activate or not.
 	$is_give_active = defined( 'GIVE_PLUGIN_BASENAME' ) ? is_plugin_active( GIVE_PLUGIN_BASENAME ) : false;
 
 	// Check to see if Give is activated, if it isn't deactivate and show a banner.
-	if ( is_admin() && current_user_can( 'activate_plugins' ) && ! $is_give_active ) {
+	if ( current_user_can( 'activate_plugins' ) && ! $is_give_active ) {
 
 		add_action( 'admin_notices', 'give_payu_inactive_notice' );
 
@@ -50,25 +48,24 @@ function give_payu_activation_banner() {
 
 	}
 
-	// Only runs on admin.
-	if ( is_admin() ) {
+	// Check for activation banner inclusion.
+	if ( ! class_exists( 'Give_Addon_Activation_Banner' )
+	     && file_exists( GIVE_PLUGIN_DIR . 'includes/admin/class-addon-activation-banner.php' )
+	) {
 
-		// Check for activation banner inclusion.
-		if ( ! class_exists( 'Give_Addon_Activation_Banner' )
-		     && file_exists( GIVE_PLUGIN_DIR . 'includes/admin/class-addon-activation-banner.php' )
-		) {
+		include GIVE_PLUGIN_DIR . 'includes/admin/class-addon-activation-banner.php';
 
-			include GIVE_PLUGIN_DIR . 'includes/admin/class-addon-activation-banner.php';
+	}
 
-		}
-
+	// Initialize activation welcome banner.
+	if ( class_exists( 'Give_Addon_Activation_Banner' ) ) {
 
 		$args = array(
 			'file'              => __FILE__,
 			'name'              => esc_html__( 'PayUmoney', 'give-payumoney' ),
 			'version'           => GIVE_PAYU_VERSION,
 			'settings_url'      => admin_url( 'edit.php?post_type=give_forms&page=give-settings&tab=gateways&section=payumoney' ),
-			'documentation_url' => 'https://github.com/WordImpress/payumoney',
+			'documentation_url' => 'http://docs.givewp.com/addon-payumoney',
 			'support_url'       => 'https://github.com/WordImpress/payumoney',
 			'testing'           => false,// Never leave true.
 		);
@@ -80,6 +77,7 @@ function give_payu_activation_banner() {
 
 }
 
+add_action( 'admin_init', 'give_payu_activation_banner' );
 
 /**
  * Notice for No Core Activation
@@ -87,7 +85,7 @@ function give_payu_activation_banner() {
  * @since 1.3.3
  */
 function give_payu_inactive_notice() {
-	echo '<div class="error"><p>' . __( '<strong>Activation Error:</strong> You must have the <a href="https://givewp.com/" target="_blank">Give</a> plugin installed and activated for the PayUmoney Add-on to activate.', 'give-payumoney' ) . '</p></div>';
+	echo '<div class="error"><p>' . __( '<strong>Activation Error:</strong> You must have the <a href="https://givewp.com/" target="_blank">Give</a> plugin installed and activated for the PayUmoney add-on to activate.', 'give-payumoney' ) . '</p></div>';
 }
 
 /**
@@ -96,5 +94,74 @@ function give_payu_inactive_notice() {
  * @since 1.3.3
  */
 function give_payu_version_notice() {
-	echo '<div class="error"><p>' . sprintf( __( '<strong>Activation Error:</strong> You must have <a href="%1$s" target="_blank">Give</a> minimum version %2$s for the PayUmoney Add-on to activate.', 'give-payumoney' ), 'https://givewp.com', GIVE_PAYU_MIN_GIVE_VER ) . '</p></div>';
+	echo '<div class="error"><p>' . sprintf( __( '<strong>Activation Error:</strong> You must have <a href="%1$s" target="_blank">Give</a> minimum version %2$s for the PayUmoney add-on to activate.', 'give-payumoney' ), 'https://givewp.com', GIVE_PAYU_MIN_GIVE_VER ) . '</p></div>';
 }
+
+
+/**
+ * Plugins row action links
+ *
+ * @since 1.0
+ *
+ * @param array $actions An array of plugin action links.
+ *
+ * @return array An array of updated action links.
+ */
+function give_payumoney_plugin_action_links( $actions ) {
+	$new_actions = array(
+		'settings' => sprintf(
+			'<a href="%1$s">%2$s</a>',
+			admin_url( 'edit.php?post_type=give_forms&page=give-settings&tab=gateways&section=payumoney' ),
+			esc_html__( 'Settings', 'give-payumoney' )
+		),
+	);
+
+	return array_merge( $new_actions, $actions );
+}
+
+add_filter( 'plugin_action_links_' . GIVE_PAYU_BASENAME, 'give_payumoney_plugin_action_links' );
+
+
+/**
+ * Plugin row meta links
+ *
+ * @since 1.0
+ *
+ * @param array  $plugin_meta An array of the plugin's metadata.
+ * @param string $plugin_file Path to the plugin file, relative to the plugins directory.
+ *
+ * @return array
+ */
+function give_payumoney_plugin_row_meta( $plugin_meta, $plugin_file ) {
+
+	if ( $plugin_file != GIVE_PAYU_BASENAME ) {
+		return $plugin_meta;
+	}
+
+	$new_meta_links = array(
+		sprintf(
+			'<a href="%1$s" target="_blank">%2$s</a>',
+			esc_url( add_query_arg( array(
+					'utm_source'   => 'plugins-page',
+					'utm_medium'   => 'plugin-row',
+					'utm_campaign' => 'admin',
+				), 'http://docs.givewp.com/addon-payumoney' )
+			),
+			esc_html__( 'Documentation', 'give-payumoney' )
+		),
+		sprintf(
+			'<a href="%1$s" target="_blank">%2$s</a>',
+			esc_url( add_query_arg( array(
+					'utm_source'   => 'plugins-page',
+					'utm_medium'   => 'plugin-row',
+					'utm_campaign' => 'admin',
+				), 'https://givewp.com/addons/' )
+			),
+			esc_html__( 'Add-ons', 'give-payu' )
+		),
+	);
+
+	return array_merge( $plugin_meta, $new_meta_links );
+}
+
+add_filter( 'plugin_row_meta', 'give_payumoney_plugin_row_meta', 10, 2 );
