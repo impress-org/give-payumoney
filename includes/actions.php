2413c84e1abad462c95e51b3fd436bf1c172dc9b
Devin Walker <devin@wordimpress.com>
2017-02-28 14:17:28 -0800

    Move action to action file

--- includes/actions.php
+++ includes/actions.php
@@ -89,3 +89,17 @@ function give_payu_add_phone_field( $form_id ) {
 
 add_action( 'give_donation_form_after_email', 'give_payu_add_phone_field' );
 
+/**
+ * Do not print cc field in donation form.
+ *
+ * Note: We do not need credit card field in donation form but we need billing detail fields.
+ *
+ * @since 1.0
+ *
+ * @param $form_id
+ */
+function give_payumoney_cc_form_callback( $form_id ) {
+	give_default_cc_address_fields( $form_id );
+}
+
+add_action( 'give_payumoney_cc_form', 'give_payumoney_cc_form_callback' );
--- includes/filters.php
+++ includes/filters.php
@@ -1,20 +1,9 @@
 <?php
 /**
- * Do not print cc field in donation form.
+ * @param $messages
  *
- * Note: We do not need credit card field in donation form but we need billing detail fields.
- *
- * @since 1.0
- *
- * @param $form_id
+ * @return mixed
  */
-function give_payumoney_cc_form_callback( $form_id ) {
-	give_default_cc_address_fields( $form_id );
-}
-
-add_action( 'give_payumoney_cc_form', 'give_payumoney_cc_form_callback' );
-
-
 function give_pauy_form_validation_message( $messages ) {
 	$messages['give_payumoney_phone'] = __( 'Please enter valid phone number without zero', 'give-payumoney' );
 
