26e11288bc4600f6093b31efeb897b329b312d57
Mehul Gohil <mehul.gohil0810@gmail.com>
2019-10-01 15:25:47 +0530

    fix: remove unnecessary fns and dependencies to improve ux

--- includes/actions.php
+++ includes/actions.php
@@ -19,32 +19,6 @@ function give_payumoney_set_donation_abandoned_callback( $payment_id ) {
 
 add_action( 'give_payumoney_set_donation_abandoned', 'give_payumoney_set_donation_abandoned_callback' );
 
-
-/**
- * Validate payumoney settings.
- *
- * @since 1.0
- *
- * @param $options
- */
-function give_payu_validate_settings( $options ) {
-	if ( isset( $options['gateways']['payumoney'] ) && ( 'INR' !== $options['currency'] ) ) {
-		// Unset payumoney.
-		unset( $options['gateways']['payumoney'] );
-
-		// Show payment gateway disable notice to admin.
-		Give_Admin_Settings::add_error(
-			'give_payu_no_inr_currency',
-			esc_html__( 'The PayUmoney payment gateway has been disabled because INR is not set as the donation currency.', 'give-payumoney' )
-		);
-
-		// Update options.
-		update_option( 'give_settings', $options );
-	}
-}
-
-add_action( 'give_save_settings_give_settings', 'give_payu_validate_settings' );
-
 /**
  * Add phone field.
  *
