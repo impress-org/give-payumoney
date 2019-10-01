23eb41e0b8e0360edba30c2c3729b96ed8d41b43
Mehul Gohil <mehul.gohil0810@gmail.com>
2019-10-01 15:34:02 +0530

    fix: show invalid currency notice when currency switcher is disabled

--- includes/actions.php
+++ includes/actions.php
@@ -86,3 +86,40 @@ function give_payumoney_cc_form_callback( $form_id ) {
 }
 
 add_action( 'give_payumoney_cc_form', 'give_payumoney_cc_form_callback' );
+
+
+/**
+ * Register Gateway Admin Notices for PayUMoney add-on.
+ *
+ * @since 1.0.5
+ *
+ * @return void
+ */
+function give_payumoney_show_admin_notice() {
+
+	// Bailout, if not admin.
+	if ( ! is_admin() ) {
+		return;
+	}
+
+	// Show currency notice, if currency is not set as "Indian Rupee".
+	if (
+		current_user_can( 'manage_give_settings' ) &&
+		'INR' !== give_get_currency() &&
+		! class_exists( 'Give_Currency_Switcher' ) // Disable Notice, if Currency Switcher add-on is enabled.
+	) {
+		Give()->notices->register_notice( array(
+			'id'          => 'give-payumoney-currency-notice',
+			'type'        => 'error',
+			'dismissible' => false,
+			'description' => sprintf(
+				__( 'The currency must be set as "Indian Rupee (₹)" within Give\'s <a href="%s">Currency Settings</a> in order to collect donations through the PayUMoney Payment Gateway.', 'give-payumoney' ),
+				admin_url( 'edit.php?post_type=give_forms&page=give-settings&tab=general&section=currency-settings' )
+			),
+			'show'        => true,
+		) );
+	}
+
+}
+
+add_action( 'admin_notices', 'give_payumoney_show_admin_notice' );
