839df54bd0a89c0064a0c36cad64dac865c58922
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-04-08 18:19:53 +0530

    doc: add comment

--- includes/lib/class-give-payumoney-api.php
+++ includes/lib/class-give-payumoney-api.php
@@ -172,6 +172,8 @@ class Give_Payumoney_API {
 	 * Get form
 	 *
 	 * @since  1.0
+	 * @unreleased add logic to submit donation form to parent when donation form is in iframe.
+	 *
 	 * @access public
 	 * @return string
 	 */
